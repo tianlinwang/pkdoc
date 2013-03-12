742d8228eae5a327372155f49e2c163db9baeab0
Tianlin Wang <tianlin.w@gmail.com>
2013-03-11 20:28:23 -0400

    modified action_list.c

--- user_manual/action_list.c
+++ user_manual/action_list.c
@@ -1,8 +1,8 @@
 /** \page action_list Action List Report
 \addindex Action List
 
-  Thsi is an over view.
-  Thsi is an over view.
+  Thsi is an over view1.
+  Thsi is an over view2.
 
 For more information, see \ref action_list_details and 
     \ref action_list_examples
